65cf7e10c0819e56e734dd8c847ae0431a6a311c
ridwanndwip <ridwandwip.rpl2@gmail.com>
2020-11-23 22:42:26 +0700

    AAdd Navbar Component

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,8 +3,23 @@ import './Navbar.scss';
 
 const Navbar = () => {
     return(
-        <div className="tes">
-            <p className="tes_p"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, velit.</p>
+        <div className="navbar">
+            <div className="navbar__logo">
+                <h4> Portofolio </h4>
+            </div>
+            <ul className="navbar__list">
+                <li className="navbar__item"><a href="#"> About </a></li>
+                <li className="navbar__item"><a href="#"> Skills </a></li>
+                <li className="navbar__item"><a href=""> Recent Works </a></li>
+                <li className="navbar__item"><a href="#"> Service </a></li>
+                <li className="navbar__item"><a href="#"> Achievement </a></li>  
+            </ul>
+            <div className="navbar__toggle">
+                <input type="checkbox"/>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
     )
 }

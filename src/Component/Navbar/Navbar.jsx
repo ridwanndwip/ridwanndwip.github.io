d2d32816d8dfd47263fa4302bfd721d479306835
ridwanndwip <ridwandwip.rpl2@gmail.com>
2020-11-23 19:19:00 +0700

    Install Node-Sass

---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.scss';
+
+const Navbar = () => {
+    return(
+        <div className="tes">
+            <p className="tes_p"> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Obcaecati, velit.</p>
+        </div>
+    )
+}
+
+export default Navbar;

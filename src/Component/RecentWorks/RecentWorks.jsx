bc59b2851729713c4d2f34487ae55f88e86ea5da
ridwanndwip <ridwandwip.rpl2@gmail.com>
2020-12-03 04:08:34 +0700

    edit recent works

--- src/Component/RecentWorks/RecentWorks.jsx
+++ src/Component/RecentWorks/RecentWorks.jsx
@@ -1,15 +1,20 @@
 import './RecentWork.scss'
-import img from '../../Assets/img/Rw1.png'
+import img from '../../Assets/img/Laper.in.png'
+import img2 from '../../Assets/img/Rivaldi-outdoor.png'
+import img3 from '../../Assets/img/indra-music.png'
+import img4 from '../../Assets/img/Mockup1.png'
+import img5 from '../../Assets/img/Untitled.png'
 
-const CardRw = ()=> {
+
+const CardRw = (props)=> {
     return(
         <div className="CardRw">
           <div className="CardRw__Overlay"></div>
-          <img src={img} alt="" className="CardRw__img"/>
+          <img src={props.img} alt="" className="CardRw__img"/>
           <div className="CardRw__detail fadeIn-bottom">
             <h3 className="wg-box-content-title">Laper.In</h3>
             <p className="wg-box-content-text"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam rem vel, illo architecto nobis nostrum.</p>
-            <a href="RIdwan.html"> View >></a>
+            <a href="RIdwan.html"> View </a>
           </div>
       </div>
     )
@@ -21,12 +26,12 @@ export default function RecentWorks(){
             <h1 data-aos="zoom-out-up" > My Recent Works </h1>
             <h5 data-aos="zoom-out-up"> Here are a few web projects and design ui/ux I've worked. Want to see more? Email me. </h5>
             <div className="rw__card">
-                <CardRw/>
-                <CardRw/>
-                <CardRw/>
-                <CardRw/>
-                <CardRw/>
-                <CardRw/>
+                <CardRw img={img}/>
+                <CardRw img={img2}/>
+                <CardRw img={img3}/>
+                <CardRw img={img4}/>
+                <CardRw img={img5}/>
+                <CardRw img={img5}/>
             </div>
         </div>
     )

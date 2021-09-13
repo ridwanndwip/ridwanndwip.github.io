c49fab12f6261ec3b7f96ed08a591914e5d78261
ridwanndwip <ridwanndwip@gmail.com>
2021-09-13 20:38:03 +0700

    add blog component

--- src/component/blog/Blog.jsx
+++ src/component/blog/Blog.jsx
@@ -4,73 +4,60 @@ import '../../style/main.scss'
 class Blog extends React.Component{
     render(){
         return(
-            <div className="container_blog">
+        <div className="container_blog">
+            
+            {/* Title */}
             <div class="title">
                 <h2 className="title_number"> 03 </h2>
                 <h4 className="title_name"> Blog </h4>
             </div>
-            <div className="blog">
 
-    <div class="row example-centered">
-        
-        <div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
-            <ul class="timeline timeline-centered">
-                <li class="timeline-item">
-                    <div class="timeline-info">
-                        <span>March 12, 2016</span>
-                    </div>
-                    <div class="timeline-marker"></div>
-                    <div class="timeline-content">
-                        <h3 class="timeline-title">Event Title</h3>
-                        <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-                            eu pede mollis pretium. Pellentesque ut neque.</p>
-                    </div>
-                </li>
-                <li class="timeline-item">
-                    <div class="timeline-info">
-                        <span>March 23, 2016</span>
-                    </div>
-                    <div class="timeline-marker"></div>
-                    <div class="timeline-content">
-                        <h3 class="timeline-title">Event Title</h3>
-                        <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-                            eu pede mollis pretium. Pellentesque ut neque. </p>
-                    </div>
-                </li>
-                <li class="timeline-item period">
-                    <div class="timeline-info"></div>
-                    <div class="timeline-marker"></div>
-                    <div class="timeline-content">
-                        <h2 class="timeline-title">April 2016</h2>
-                    </div>
-                </li>
-                <li class="timeline-item">
-                    <div class="timeline-info">
-                        <span>April 02, 2016</span>
-                    </div>
-                    <div class="timeline-marker"></div>
-                    <div class="timeline-content">
-                        <h3 class="timeline-title">Event Title</h3>
-                        <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-                            eu pede mollis pretium. Pellentesque ut neque. </p>
-                    </div>
-                </li>
-                <li class="timeline-item">
-                    <div class="timeline-info">
-                        <span>April 28, 2016</span>
-                    </div>
-                    <div class="timeline-marker"></div>
-                    <div class="timeline-content">
-                        <h3 class="timeline-title">Event Title</h3>
-                        <p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-                            eu pede mollis pretium. Pellentesque ut neque. </p>
-                    </div>
-                </li>
-            </ul>
+            {/* Blog */}
+            <div className="blog">
+                <ul class="timeline_blog">
+                    <li class="blog_item">
+                        <div class="timeline-info">
+                            <span>March 12, 2016</span>
+                        </div>
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                            <p class="timeline-title">Lorem ipsum dolor sit amet ctur adipisicing elit. Officiis, voluptatem.</p>
+                            <a href="#"> Lihat selengkapnya </a>
+                        </div>
+                    </li>
+                    <li class="blog_item">
+                        <div class="timeline-info">
+                            <span>March 12, 2016</span>
+                        </div>
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                            <p class="timeline-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.</p>
+                            <a href="#"> Lihat selengkapnya </a>
+                        </div>
+                    </li>
+                    <li class="blog_item">
+                        <div class="timeline-info">
+                            <span>March 12, 2016</span>
+                        </div>
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                            <p class="timeline-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.</p>
+                            <a href="#"> Lihat selengkapnya </a>
+                        </div>
+                    </li>
+                    <li class="blog_item">
+                        <div class="timeline-info">
+                            <span>March 12, 2016</span>
+                        </div>
+                        <div class="timeline-marker"></div>
+                        <div class="timeline-content">
+                            <p class="timeline-title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, voluptatem.</p>
+                            <a href="#"> Lihat selengkapnya </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
-            </div>
-            </div>
         )
     }
 }

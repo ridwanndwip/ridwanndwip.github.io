dc71f0a8ae1c95f55a43772606761e9f230dc73b
ridwanndwip <ridwanndwip@gmail.com>
2021-09-14 10:33:32 +0700

    add loading spinner screen

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import react, {useState, useEffect} from 'react';
+import DotLoader from "react-spinners/DotLoader";
 import Navbar from './component/navbar/Navbar';
 import Home from './component/home/Home';
 import Me from './component/me/Me';
@@ -5,17 +7,33 @@ import Work from './component/work/Work';
 import Blog from './component/blog/Blog';
 import Contact from './component/contact/Contact';
 import React from 'react';
+import './App.css'
 
 function App() {
+  const [loading,setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 4000)
+  }, [])
   return (
-    <React.Fragment>
-      <Navbar/>
-      <Home/>
-      <Me/>
-      <Work/>
-      <Blog/>
-      <Contact/>
-    </React.Fragment>
+    <div className={loading ? 'App' : 'noneApp'}>
+      { 
+        loading ?
+        <DotLoader color={"#ffffff"} loading={loading} size={70} />
+        :
+        <div>
+          <Navbar/>
+          <Home/>
+          <Me/>
+          <Work/>
+          <Blog/>
+          <Contact/>
+        </div>
+      }
+    </div>
   )}
 
 export default App;
--- src/component/contact/Contact.jsx
+++ src/component/contact/Contact.jsx
@@ -4,7 +4,20 @@ import '../../style/main.scss'
 class Contact extends React.Component{
     render(){
         return(
-            <h1> contact </h1>
+            <div className="container_contact">
+            <div class="title">
+                <h2 className="title_number"> 04 </h2>
+                <h4 className="title_name"> Ask </h4>
+            </div>
+            <div className="contact">
+                <h2> title Here </h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Placeat non suscipit iusto numquam dignissimos consequatur magni labore rerum alias cumque, neque iste illo incidunt sequi explicabo praesentium dolorem ullam ipsum.</p>
+            </div>
+            <div className="footer">
+                <h4> Design & Built by Ridwanganteng</h4>
+                <h6> Live to learn and leveling up 🍁</h6>
+            </div>
+            </div>
         )
     }
 }
